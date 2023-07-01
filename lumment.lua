943be1732a570e9d7db98dc0fa20508310d03183
Pauyy <pauyyk@gmail.com>
2023-07-01 17:56:41 +0200

    Add idea to loop through entire directories

--- lumment.lua
+++ lumment.lua
@@ -1,12 +1,22 @@
-
-content = io.popen("dir /b /a-d")
-for line in content:lines() do
-	print(line)
+local complete = {}
+local base_path = ""
+--TODO Fix
+function scan_directory(path)
+	local directories = io.popen(string.format("dir %s /b /ad", path))
+	for directoriename in directories:lines() do
+		scan_directory(path .. "/" .. directoriename)
+	end
+	directories:close()
+	local files = io.popen(string.format("dir %s *.java* *.h* /b /a-d", path))
+	for filename in files:lines() do
+		print(filename)
+		table.insert(complete, dofile("main.lua")(path .. "/" .. filename))
+	end
+	files:close()
 end
-content:close()
 
-comments = loadfile("main.lua")("test.java")
+scan_directory(base_path)
 
-for k,v in ipairs(comments) do
+for k,v in ipairs(complete) do
  print(v)
  end
