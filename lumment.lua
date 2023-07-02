b8f5e349219a8b7cb1c193ebb4ca672aee99b77a
Pauyy <pauyyk@gmail.com>
2023-07-02 13:04:47 +0200

    Make finding every file with .java .c* or .h* actually work

--- lumment.lua
+++ lumment.lua
@@ -9,7 +9,7 @@ function scan_directory(path, i)
 		scan_directory(path .. "\\" .. directoriename, i+1)
 	end
 	directories:close()
-	local files = io.popen(string.format("dir %s\\*.java* *.h* /b /a-d-h 2>nul", path))
+	local files = io.popen(string.format('dir %s /b /a-d-h 2>nul | findstr /l ".java .c .h"', path))
 	for filename in files:lines() do
 		--print("file",i, path, filename, path .. "\\" .. filename)
 		table.insert(complete, find_comment(path .. "\\" .. filename))
