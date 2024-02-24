4799a1bf7198ce902306fa2b023b33a070768bd6
Pauyy <pauyyk@gmail.com>
2024-02-24 12:16:48 +0100

    Rename main.lua to check_file.lua

--- lumment.lua
+++ lumment.lua
@@ -5,7 +5,7 @@ local relative_path = arg[0]:sub(1, last_slash or 0)
 
 local base_path = arg[1] or os.getenv("PWD") or io.popen("cd"):read()
 
-local find_comment = assert(loadfile(relative_path .. "main.lua"))
+local find_comment = assert(loadfile(relative_path .. "check_file.lua"))
 
 local lummentignore = io.open(relative_path .. ".lummentignore", "r")
 local ignored_folders = {}
